7e55f35a762177e223be07372acfff5be69d7d7d
tigran <tigran@blockchainrndhub.com>
2018-08-17 18:03:08 +0400

    fix: warning

--- src/test_files_diff/main.cpp
+++ src/test_files_diff/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include <iostream>
 #include <boost/filesystem.hpp>
+
+#include <belt.pp/global.hpp>
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
 
-
+    B_UNUSED(argc);
+    B_UNUSED(argv);
 
    cout<<argv[1]<<argv[2]<<endl;
 
