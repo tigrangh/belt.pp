9842d57603d859c15a6f70ba6a46eaffa3e18685
Tigran Ghahramanyan <tghahram@gmail.com>
2018-12-24 15:33:44 +0400

    trying variants of sessions

--- src/belt.pp/session_manager.hpp
+++ src/belt.pp/session_manager.hpp
@@ -13,31 +13,16 @@
 
 namespace beltpp
 {
+template <typename T_key, typename T_value>
 class session
 {
-    using string = std::string;
     using packet = ::beltpp::packet;
     using time_point = ::std::chrono::steady_clock::time_point;
 public:
-    packet package_expected;
-    packet package_state;
-private:
-    time_point discard_time;
-};
-
-class session_manager
-{
-    using string = ::std::string;
-    using duration = ::std::chrono::steady_clock::duration;
-public:
-    void set_processor(std::function<void()> const& processor);
-    session& store(string const& peerid, duration const& active);
-    void drop(string const& peerid);
-    bool is_stored(string const& peerid);
-    bool take_to_remove(string& peerid);
-private:
-    std::unordered_map<string, session> sessions;
-
+    size_t expected_package_type;
+    T_key key;
+    T_value value;
+    time_point discard_after;
 };
 }
 
